b5359947acf9b03cfa916c42bccbe9dc57ad403c
Booker Robinson <robinson.booker11@gmail.com>
2019-02-07 11:40:31 -0400

    add server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express"),
+      app     = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.sendFile("index.html");
+});
+
+app.listen(8000, process.env.IP, function(){
+    console.log("server started...");
+});
